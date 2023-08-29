e2efb74e2583d2ebd433f7ce04b925f4d5bf247d
Erika Estrada <edem1228@gmail.com>
2023-08-28 17:12:42 -0700

    Added two GET routes to get a member's friends.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import members from "../data/members.json" assert { type: "json" };
+import friendships from "../data/friendships.json" assert { type: "json" };
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +15,30 @@ app.get("/member/:id", (req, res) => {
     const memberId = Number(req.params.id);
     const member = members.filter((member) => member.id === memberId);
     res.send(member);
+});
+
+// GET member's friends by member ID
+app.get("/member/:id/friends", (req, res) => {
+    const memberId = Number(req.params.id);
+    const filteredFriendships = friendships.filter((friendship) => friendship.member1_id === memberId);
+    const friends = filteredFriendships.map((friendship) =>
+        members.filter((member) => member.id === friendship.member2_id)
+    );
+
+    res.send(friends);
+});
+
+// GET a member's friend (returns empty set if the two members are not friends)
+app.get("/member/:id1/friends/:id2", (req, res) => {
+    const memberId = Number(req.params.id1);
+    const friendId = Number(req.params.id2);
+    const friendship = friendships.filter((friendship) =>
+        (friendship.member1_id === memberId && friendship.member2_id === friendId)
+        || (friendship.member1_id === friendId && friendship.member2_id === memberId)
+    );
+    const friend = friendship.length > 0 ? members.filter((member) => member.id === friendId) : [];
+
+    res.send(friend);
 });
 
 app.listen(PORT, () => {
