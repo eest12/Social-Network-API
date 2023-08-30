8e35d2dc5a5384f60e9adca5143d4e72f924ab76
Erika Estrada <edem1228@gmail.com>
2023-08-29 22:31:25 -0700

    Added a POST route to register a new member.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from "express";
+import fs from "fs";
 import members from "../data/members.json" assert { type: "json" };
 import friendships from "../data/friendships.json" assert { type: "json" };
 
 const app = express();
 const PORT = 3000;
+
+// Receive requests in JSON format
+app.use(express.json());
 
 // GET all members
 app.get("/members", (req, res) => {
@@ -39,6 +43,41 @@ app.get("/member/:id1/friends/:id2", (req, res) => {
     const friend = friendship.length > 0 ? members.filter((member) => member.id === friendId) : [];
 
     res.send(friend);
+});
+
+// POST new member
+app.post("/register", (req, res) => {
+    const newId = members[members.length - 1].id + 1;
+    const requiredFields = ["username", "first_name", "last_name", "email", "gender", "birthdate", "avatar"];
+
+    // validate request body fields
+    for (let i = 0; i < requiredFields.length; i++) {
+        if (req.body[requiredFields[i]] == null) {
+            return res.sendStatus(400);
+        }
+    }
+
+    const newMember = {
+        id: newId,
+        username: req.body.username,
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        gender: req.body.gender,
+        birthdate: req.body.birthdate,
+        avatar: req.body.avatar
+    };
+
+    const updatedMembers = members.concat(newMember);
+    const updatedMembersJson = JSON.stringify(updatedMembers);
+
+    try {
+        fs.writeFileSync("data/members.json", updatedMembersJson);
+        res.sendStatus(201);
+    } catch (e) {
+        console.log(e);
+        res.sendStatus(400);
+    }
 });
 
 app.listen(PORT, () => {
